b9be09d76c58d037883675d06d8c285ceeaa9345
KyllianBeguin <KyllianBeguin@vivaldi.net>
2024-12-02 23:02:37 +0100

    [kb] 🔨 First half

--- 2024/day-01/python/solution.py
+++ 2024/day-01/python/solution.py
@@ -1,1 +1,16 @@
 import os
+
+path = os.getcwd().replace("python", ".input")
+
+# Parse input
+ipt = [i.replace('\n', '').split('   ') for i in open(path, 'r').readlines()]
+l1: list = [eval(i[0]) for i in ipt]
+l2: list = [eval(i[1]) for i in ipt]
+
+# Order lists
+l1.sort()
+l2.sort()
+
+dist = [abs(v - l2[i]) for i, v in enumerate(l1)]
+
+print(sum(dist))
