d7d356ca365d476fc40da9819db89d87a71af0d7
KyllianBeguin <KyllianBeguin@vivaldi.net>
2024-12-02 23:54:20 +0100

    [kb] 🔨 First half

--- 2024/day-02/python/solution.py
+++ 2024/day-02/python/solution.py
@@ -1,1 +1,15 @@
 import os
+
+path = os.getcwd().replace("python", ".input")
+
+# Parse input
+ipt: list = [[eval(j) for j in i.replace('\n', '').split(' ')] for i in open(path, 'r').readlines()]
+
+safe_updown: list = [i for i in ipt if len(set(['up' if v-i[j+1] > 0 else 'down' for j, v in enumerate(i[:-1])])) == 1]
+
+safe_incr: list = [i for i in safe_updown if len(set(['ok' if (abs(v-i[j+1]) <=3 and abs(v-i[j+1]) > 0)  else 'ko' for j, v in enumerate(i[:-1])])) == 1]
+
+print(len(safe_incr))
+
+#tstsafe_incr: list = [[abs(v-i[j+1]) for j, v in enumerate(i[:-1])] for i in safe_updown]
+#print(tstsafe_incr)
