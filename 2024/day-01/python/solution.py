2f4fd70185f38a814c97dd28bff911ac84e82ce7
KyllianBeguin <KyllianBeguin@vivaldi.net>
2024-12-02 23:11:09 +0100

    [kb] 🔨 Second half

--- 2024/day-01/python/solution.py
+++ 2024/day-01/python/solution.py
@@ -3,14 +3,20 @@ import os
 path = os.getcwd().replace("python", ".input")
 
 # Parse input
-ipt = [i.replace('\n', '').split('   ') for i in open(path, 'r').readlines()]
+ipt: list = [i.replace('\n', '').split('   ') for i in open(path, 'r').readlines()]
 l1: list = [eval(i[0]) for i in ipt]
 l2: list = [eval(i[1]) for i in ipt]
 
+# PART ONE =====
 # Order lists
 l1.sort()
 l2.sort()
 
-dist = [abs(v - l2[i]) for i, v in enumerate(l1)]
+dist:list = [abs(v - l2[i]) for i, v in enumerate(l1)]
 
-print(sum(dist))
+# PART TWO =====
+
+# Detect numbers from l1 in l2
+det: list = [i * l2.count(i) for i in l1 if i in l2]
+
+print(sum(det))
